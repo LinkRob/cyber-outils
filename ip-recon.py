12558b795f269d0abcb6f1c1b3663fb18b9af1ac
LinkRob <robleteric@gmail.com>
2026-06-12 17:58:56 +0000

    Ajout du scanner de port 80

--- ip-recon.py
+++ ip-recon.py
@@ -8,3 +8,14 @@ try:
     print(f"[+] Adresse IP de la cible trouvée : {ip}")
 except socket.gaierror:
     print("[-] Impossible de résoudre le nom de domaine.")
+print("[*] Vérification du port 80 (Web)...")
+scanner = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+scanner.settimeout(2)
+
+code_port = scanner.connect_ex((ip, 80))
+
+if code_port == 0:
+    print("[+] Port 80 : OUVERT !")
+else:
+    print("[-] Port 80 : FERMÉ.")
+scanner.close()
